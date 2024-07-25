09226da591d4e17879329c42ee9459f89ef14b1f
AmirSina Mashayekh <amirsina.mashayekh@outlook.com>
2024-07-25 23:25:28 +0330

    Add: consume_whitespace, match_iden

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Error, fs::File, io::{self, BufReader, Read}};
+use std::io::{self, BufReader, Read};
 
 use token::Token;
 use token_type::TokenType;
@@ -23,6 +23,25 @@ impl<R: Read> Lexer <R> {
         }
     }
 
+    fn consume_whitespace(&mut self) -> io::Result<()> {
+        while self.current.is_whitespace() {
+            self.advance()?;
+        }
+
+        Ok(())
+    }
+
+    fn match_iden(&mut self) -> io::Result<String> {
+        let mut id = String::new();
+
+        while self.current.is_ascii_alphanumeric() || self.current == '_' {
+            id.push(self.current);
+            self.advance()?;
+        }
+
+        Ok(id)
+    }
+
     fn next(&mut self) -> io::Result<char> {
         let mut buf = [0 as u8];
         
@@ -31,5 +50,12 @@ impl<R: Read> Lexer <R> {
             Ok(_) => Ok(buf[0] as char),
             Err(e) => Err(e),
         }
+    }
+
+    fn advance(&mut self) -> io::Result<()> {
+        let c = self.next()?;
+        self.current = c;
+
+        Ok(())
     }
 }
